fe4607170ff9cfdaa1c2d7fa08371f01d0ae7ea6
seonyo <jsy6535@naver.com>
2023-11-02 01:45:30 +0900

    ✨[Feat] 아이디 중복 값 입력 예외 추가

--- server_Conn/joinus.js
+++ server_Conn/joinus.js
@@ -10,22 +10,27 @@ submitBtn.onclick = () => {
     let pwCheck = checkField.value;
     let name = nameField.value;
 
-    if(pw === pwCheck){
+    if (pw === pwCheck) {
 
         let userData = {
-            user_id : id,
-            user_pw : pw,
-            user_name : name
+            user_id: id,
+            user_pw: pw,
+            user_name: name
         }
 
         axios.post('http://localhost:3000/user', userData)
-        .then(response => {
-            console.log('Registration successful:', response.data);
-            window.location.href = 'signin.html';
-        })
-        .catch(error => {
-            console.error('Registration failed:', error.response.data);
-        })        
+            .then(response => {
+                console.log('Registration successful:', response.data);
+                window.location.href = 'signin.html';
+            })
+            .catch(error => {
+                if(error.response.status === 404){
+                    alert('중복된 아이디 값이 있습니다')
+                }
+                else {
+                    console.error('Registration failed:', error.response.data);
+                }
+            })
     }
 
 }
