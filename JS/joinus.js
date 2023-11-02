bc25f11ec1a66578fa81af2b2f8ec664dfa37f60
seonyo <jsy6535@naver.com>
2023-11-02 14:02:33 +0900

    ✨[Feat] id 유효성검사

--- JS/joinus.js
+++ JS/joinus.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const passwordInput = document.getElementById("pw");
     const passwordCheckInput = document.getElementById("pw-check");
     const nameInput = document.getElementById("name");
+    const idLabel = document.getElementById("inputLabel");
 
     idInput.addEventListener("click", function() {
         if (idInput.value === "아이디") {
@@ -13,6 +14,18 @@ document.addEventListener("DOMContentLoaded", function() {
     idInput.addEventListener("blur", function() {
         if (idInput.value === "") {
             idInput.value = "아이디";
+        }
+        
+        //아이디 유효성 검사
+        let reg_id = /^[a-z0-9_-]{6,20}$/
+        if(!reg_id.test(idInput.value)){
+            idInput.style.outline ="1px solid #FF3B30";
+            idLabel.style.color = "#FF3B30"
+        }
+
+        else {
+            idInput.style.outline ="1px solid #34C759";
+            idLabel.style.color = "#34C759" 
         }
     });
 
